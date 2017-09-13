52568b72664926c660af35cf05e30f485014cc5a
Tim Peacock <tim.peacock@jazzmine.plus.com>
2017-09-13 12:53:06 +0100

    Formatting only:  Regularise script formatting.

--- script/weather.js
+++ script/weather.js
@@ -1,21 +1,21 @@
-
-(function() {
+( function() {
 
   'use strict';
 
   function error() {
-    console.log( 'Got error from data returned.');
+    console.log( 'Got error from data returned.' );
   }
 
-  function dataReceived(e) {
+  function dataReceived( e ) {
 
     const data = JSON.parse( this.response ) || {};
 
-    let el = document.getElementById('current-time');
+    let el = document.getElementById( 'current-time' );
 
-    if (el && data.dt) {
+    if ( el && data.dt ) {
       // Date is in seconds, not milliseconds.
-      const dateString = new Date( data.dt ).toLocaleTimeString();
+      const dateString = new Date( data.dt )
+        .toLocaleTimeString();
       el.textContent = dateString;
     }
 
@@ -48,15 +48,18 @@
   function getData() {
     const req = new XMLHttpRequest();
 
-    req.addEventListener('load', dataReceived);
-    req.addEventListener('error', function() {console.log('error from request');})
+    req.addEventListener( 'load', dataReceived );
+    req.addEventListener( 'error', function() {
+      console.log( 'error from request' );
+    } )
 
-    req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?units=metric&id=2643743&APPID=f6347fd66d842b5f22cec8fb8851e4f8');
+    req.open( 'GET', 'http://api.openweathermap.org/data/2.5/weather?units=metric&id=2643743&APPID=f6347fd66d842b5f22cec8fb8851e4f8' );
 
-    req.setRequestHeader('Accept', 'application/json');
+    req.setRequestHeader( 'Accept', 'application/json' );
     req.send();
   }
 
-  document.getElementById('go-button').addEventListener('click', getData);
+  document.getElementById( 'go-button' )
+    .addEventListener( 'click', getData );
 
-})()
+} )()
